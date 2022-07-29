f5e7af3636c9272954e277d1db78e662eabbf18f
Prasanth239 <prasanth@ikshalabs.com>
2022-07-29 18:43:50 +0530

    first commit

---
+++ ExpenseTracker/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+
+const EXPENSES = [];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
